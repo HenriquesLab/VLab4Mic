330fc29b5d6e998198a0581019183db40a593fa3
Damian Martinez <jdmartinezigc@gmail.com>
2024-10-03 14:21:11 +0100

    test: created tests for defects with param sweep

---
+++ tests/test_feature_defects.py
@@ -0,0 +1,34 @@
+from supramolsim.utils.data_format.structural_format import label_builder_format
+import pytest
+import numpy as np
+from supramolsim import workflows
+
+defects = np.linspace(0, 1, 27)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def labelled_particle(configuration_directory):
+    structure_id = "7R5K"
+    structure_label = "7R5K_Nup96_Cterminal_direct"
+    configuration_path = configuration_directory
+    fluorophore_id = "AF647"
+    structure, structure_param = workflows.load_structure(
+        structure_id, configuration_path
+    )
+    labels_list = []
+    labels_list.append(label_builder_format(structure_label, fluorophore_id))
+    particle = workflows.particle_from_structure(
+        structure, labels_list, configuration_path
+    )
+    return particle
+
+
+@pytest.mark.parametrize("defect", defects)
+def test_labeff_on_specific_labels(defect, labelled_particle):
+    assert labelled_particle.get_ref_point().shape == (3,)
+    labelled_particle.add_defects(
+        eps1=300,
+        xmer_neigh_distance=600,
+        deg_dissasembly=defect,
+    )
+    assert labelled_particle.defects_target_normals is not None
