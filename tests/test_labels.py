4225a791e2cb1ea9cbbecf1d123918e94f76f7b5
Damian Martinez <jdmartinezigc@gmail.com>
2025-02-18 17:36:19 +0000

    added test for building probe model with moch antibody

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -1,4 +1,6 @@
 from supramolsim.generate import labels
+from supramolsim.workflows import probe_model
+from supramolsim.utils.io.yaml_functions import load_yaml
 import os
 
 
@@ -12,3 +14,14 @@ def test_createlabel_from_config(configuration_directory):
     label_object, label_params = labels.construct_label(
         local_path_congfig, fluorophoreID, labelling_efficiency
     )
+
+
+def test_probe_model(configuration_directory):
+    label_file = "Mock_primary_antibody" + ".yaml"
+    local_path_congfig = os.path.join(configuration_directory, "probes", label_file)
+    probe_params = load_yaml(local_path_congfig)
+    probe, probe_emitters, anchor, ab_ref = probe_model(
+        **probe_params, config_dir=configuration_directory
+    )
+    assert anchor.shape == (3,)
+    assert ab_ref.shape == (3,)
