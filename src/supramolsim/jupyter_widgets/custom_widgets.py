38c0368c5d3cdc9fd706a22067709e9fae50627a
Damian Martinez <jdmartinezigc@gmail.com>
2025-03-05 13:08:41 +0000

    added checkbox for using ab as linker

--- src/supramolsim/jupyter_widgets/custom_widgets.py
+++ src/supramolsim/jupyter_widgets/custom_widgets.py
@@ -298,8 +298,9 @@ def create_structural_model():
                 type=labels_gui["mock_type"].value,
                 value=target_value
             )
+            as_linker = labels_gui["as_linker"].value
             tmp_label = data_format.structural_format.label_builder_format(
-                label_id, fluorophore_id, lab_eff, target_info
+                label_id, fluorophore_id, lab_eff, target_info, as_linker
             )
             unique_name = label_id + "_conjugated_" + fluorophore_id
             if unique_name in current_labels.keys():
@@ -366,8 +367,8 @@ def create_structural_model():
 
         labels_gui.add_label("Mock labels")
         labels_gui.add_dropdown("mock_label_dropdown", options=mock_labels)
-        labels_gui.add_dropdown("mock_type", options=["Sequence", "Atom_residue"])
-        labels_gui.add_textarea("mock_value")
+        labels_gui.add_dropdown("mock_type", options=["Sequence", "Atom_residue", "Primary"],description="Target type: ")
+        labels_gui.add_textarea("mock_value", description="Target value: ")
         labels_gui.add_dropdown("mock_fluo_dropdown", options=fluorophores_list)
         labels_gui.add_float_slider(
             "mock_Labelling_efficiency",
@@ -377,6 +378,8 @@ def create_structural_model():
             step=0.01,
             description="Labelling efficiency",
         )
+        labels_gui.add_checkbox("as_linker", description = "Model as primary with epitope for secondary probe",
+                        value = False)
         labels_gui.add_button("Add_mock", description="Add mock label")
         labels_gui["Add_mock"].on_click(build_mock_label)
 
