b62f243898e21ddcb41e0ead1e810f870630d264
Damian Martinez <jdmartinezigc@gmail.com>
2025-03-05 13:56:03 +0000

    inlcude tests for primary ab as linkers or conjugated and updated assertions

--- tests/test_sequential_labelling.py
+++ tests/test_sequential_labelling.py
@@ -2,6 +2,39 @@ import pytest
 from supramolsim import workflows, data_format
 
 
+probes_1XI5 = []
+target_info1 = dict(
+    type="Sequence",
+    value="EQATETQ"
+)
+fluorophore_id1 = "AF647"
+lab_eff1 = 1
+linker1 = data_format.structural_format.label_builder_format(
+    "Mock_linker", fluorophore_id1, lab_eff1, target_info1
+)
+antibody = data_format.structural_format.label_builder_format(
+    "Mock_antibody", fluorophore_id1, lab_eff1, target_info1, as_linker=False
+)
+antibody_as_linker = data_format.structural_format.label_builder_format(
+    "Mock_antibody", fluorophore_id1, lab_eff1, target_info1, as_linker=True
+)
+probes_1XI5 = [linker1, antibody, antibody_as_linker]
+
+@pytest.mark.parametrize("probe_primary", probes_1XI5)
+def test_primaries_only(configuration_directory, probe_primary):
+    structure_id = "1XI5"
+    structure, structure_param = workflows.load_structure(
+        structure_id, configuration_directory
+    )
+    labels_list = [probe_primary,]
+    particle, label_params_list = workflows.particle_from_structure(
+        structure, labels_list, configuration_directory
+    )
+    assert particle.get_ref_point().shape == (3,)
+
+
+def test_primary_secondary(configuration_directory):
+    pass
 
 
 def test_linker_secondary(configuration_directory):
@@ -20,26 +53,23 @@ def test_linker_secondary(configuration_directory):
         label_id1, fluorophore_id1, lab_eff1, target_info1
     )
 
-    label_id2 = "Mock_primary_antibody"
+    label_id2 = "Mock_antibody"
     target_info2 = dict(
         type="Primary",
         value="Mock_linker"
     )
-    fluorophore_id2 = "AF647"
+    fluorophore_id2 = "AF488"
     lab_eff2 = 1
     tmp_label2 = data_format.structural_format.label_builder_format(
-        label_id2, fluorophore_id2, lab_eff2, target_info2
+        label_id2, fluorophore_id2, lab_eff2, target_info2, as_linker=False
     )
     particle2, label_params_list = workflows.particle_from_structure(
         structure, [tmp_label1, tmp_label2], configuration_directory
     )
     assert len(particle2.secondary.keys()) > 0
-    first_labels = particle2.emitters[target_info2["value"]].shape
-    particle2.sequential_labelling=True
-    particle2.generate_instance()
-    second_labels = particle2.emitters[target_info2["value"]].shape
-    assert first_labels[0] != second_labels[0]
-
+    epitopes = particle2.source["targets"][label_id1]["coordinates"].shape
+    emitters = particle2.emitters[label_id1].shape
+    assert epitopes != emitters
 
 
 def test_primary_secondary(configuration_directory):
@@ -47,7 +77,7 @@ def test_primary_secondary(configuration_directory):
     structure, structure_param = workflows.load_structure(
         structure_id, configuration_directory
     )
-    label_id1 = "Mock_primary_antibody"
+    label_id1 = "Mock_antibody"
     target_info1 = dict(
         type="Sequence",
         value="EQATETQ"
@@ -55,15 +85,15 @@ def test_primary_secondary(configuration_directory):
     fluorophore_id1 = "AF647"
     lab_eff1 = 1
     tmp_label1 = data_format.structural_format.label_builder_format(
-        label_id1, fluorophore_id1, lab_eff1, target_info1
+        label_id1, fluorophore_id1, lab_eff1, target_info1, as_linker=True
     )
 
-    label_id2 = "Mock_primary_antibody"
+    label_id2 = "Mock_antibody"
     target_info2 = dict(
         type="Primary",
         value="Mock_antibody"
     )
-    fluorophore_id2 = "AF647"
+    fluorophore_id2 = "AF488"
     lab_eff2 = 1
     tmp_label2 = data_format.structural_format.label_builder_format(
         label_id2, fluorophore_id2, lab_eff2, target_info2
@@ -72,8 +102,6 @@ def test_primary_secondary(configuration_directory):
         structure, [tmp_label1, tmp_label2], configuration_directory
     )
     assert len(particle2.secondary.keys()) > 0
-    first_labels = particle2.emitters[target_info2["value"]].shape
-    particle2.sequential_labelling=True
-    particle2.generate_instance()
-    second_labels = particle2.emitters[target_info2["value"]].shape
-    assert first_labels[0] != second_labels[0]
+    epitopes = particle2.source["targets"][label_id1]["coordinates"].shape
+    emitters = particle2.emitters[label_id1].shape
+    assert epitopes != emitters
