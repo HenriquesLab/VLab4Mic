4bad1a6e6349eec2cf20ddea7f10a7a3ecb4e85d
Damian Martinez <jdmartinezigc@gmail.com>
2025-03-05 13:17:41 +0000

    fixed missing target type option

--- src/supramolsim/jupyter_widgets/custom_widgets.py
+++ src/supramolsim/jupyter_widgets/custom_widgets.py
@@ -286,14 +286,14 @@ def create_structural_model():
             label_id = labels_gui["mock_label_dropdown"].value
             fluorophore_id = labels_gui["mock_fluo_dropdown"].value
             lab_eff = labels_gui["mock_Labelling_efficiency"].value
-            if labels_gui["mock_type"].value == "Sequence":
-                target_value = labels_gui["mock_value"].value
-            elif labels_gui["mock_type"].value == "Atom_residue":
+            if labels_gui["mock_type"].value == "Atom_residue":
                 atom, residue = labels_gui["mock_value"].value.split(".")
                 target_value = dict(
                     atoms=atom,
                     residues=residue
                 )
+            else: # can be Sequence or Primary, both cases is a string
+                target_value = labels_gui["mock_value"].value
             target_info=dict(
                 type=labels_gui["mock_type"].value,
                 value=target_value
