2577bb11ef2c14ef93fd666eb6cfc0c9f36eddb7
Damian Martinez <jdmartinezigc@gmail.com>
2025-03-26 18:45:57 +0000

    test: added test for base generator of virtual sample

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,5 +1,4 @@
-from supramolsim.experiments import create_experiment_parametrisation
-
+from supramolsim import experiments 
 
 def test_experiment_parameterised():
     selected_mods = dict(
@@ -17,7 +16,7 @@ def test_experiment_parameterised():
         defects=dict(start=0, end=0.5, nintervals=3),
     )
     savging = dict(experiment_id="SupraMolSim_experiment", output_directory="")
-    experiment_generator = create_experiment_parametrisation(
+    experiment_generator = experiments.create_experiment_parametrisation(
         structure_and_labels=structure_and_labels,
         modalities_acquisition=selected_mods,
         defects_params=defects_eps_d,
@@ -37,9 +36,15 @@ def test_experiment_runsimulation():
         fluorophore_id="AF647",
     )
     savging = dict(experiment_id="SupraMolSim_experiment", output_directory="")
-    experiment_generator = create_experiment_parametrisation(
+    experiment_generator = experiments.create_experiment_parametrisation(
         structure_and_labels=structure_and_labels,
         modalities_acquisition=selected_mods,
         savging=savging,
     )
     experiment_generator.run_simulation()
+
+
+def test_gen_virtual_sample():
+    vsample, testexperiment = experiments.generate_virtual_sample()
+    print(vsample)
+    assert len(vsample["reference_point"]) == 3
