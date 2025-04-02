9483825b2ad5e1e281eb26c245f9796cef5624bc
Damian Martinez <jdmartinezigc@gmail.com>
2025-04-02 16:32:10 +0100

    tests: added tests for parameter generators

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -135,3 +135,44 @@ def test_sweep_vsample_modality_analysis():
     df_categories, titles = sweep.pivot_dataframes_byCategory(
         combined, "modality", "labelling_efficiency", "distance_to_epitope"
     )
+
+def test_sweep_modalities_updatemod():
+    myexperiment, vsmpl_output, vsampl_pars = sweep.sweep_vasmples()
+    myexperiment, mod_outputs, mod_params, mod_pixelsizes  = sweep.sweep_modalities_updatemod(
+        experiment = myexperiment,
+        vsample_outputs = vsmpl_output,
+        vsampl_pars = vsampl_pars,
+        )
+
+
+def test_parameter_generators():
+    structures = ["1XI5", ]
+    probes=["NHS_ester", ]
+    modalities = ["STED",]
+    probe_parameters = sweep.probe_parameters_sweep(labelling_efficiency=[0.5,1,1])
+    vsample_parameters = sweep.virtual_sample_parameters_sweep(random_orientations=[False,])
+    modality_parameters = sweep.modality_parameters_sweep(lateral_resolution_nm=[10,100,1])
+    acquisition_params = sweep.acquisition_parameters_sweep(exp_time=[0.001, 0.1, 1])
+    ref_probe_parameters = dict(
+        target_info = dict(type="Sequence", value="EQATETQ"),
+        labelling_efficiency = 1,
+        distance_to_epitope = 0
+    )
+    ref_vsample, ref_params = sweep.generate_global_reference_sample(structure=structures[0], probe="Mock_linker", probe_parameters=ref_probe_parameters)
+    ref_image, ref_image_pars = sweep.generate_global_reference_modality(reference_vsample=ref_vsample, reference_vsample_params=ref_params)
+    myexperiment, vsmpl_output, vsampl_pars = sweep.sweep_vasmples(
+        structures=structures,
+        probes=probes, 
+        probe_parameters=probe_parameters,
+        virtual_samples=vsample_parameters,
+        repetitions=3
+    )
+    myexperiment, mod_outputs, mod_params, mod_pixelsizes  = sweep.sweep_modalities_updatemod(
+        experiment = myexperiment,
+        vsample_outputs = vsmpl_output,
+        vsampl_pars = vsampl_pars,
+        modalities=modalities,
+        modality_params=modality_parameters,
+        modality_acq_prams=acquisition_params
+    )
+    measurements, inputs = sweep.analyse_sweep_single_reference(mod_outputs, mod_params, ref_image[0], ref_image_pars)
