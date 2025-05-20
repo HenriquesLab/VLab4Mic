cbb95dfce3298f66721f297c8d477733cb239a93
Damian Martinez <jdmartinezigc@gmail.com>
2025-05-20 14:40:04 +0100

    remove redundant test for indirect probes

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -43,41 +43,6 @@ labelling_efficiency = np.linspace(0.5, 1, 3)
 distance_to_epitope = np.linspace(10, 200, 3)
 
 
-@pytest.mark.parametrize("indirectprobe", indirectprobes)
-@pytest.mark.parametrize("efficiency", labelling_efficiency)
-@pytest.mark.parametrize("distance", distance_to_epitope)
-def test_sweep_vasmples_indirectprobes(indirectprobe, efficiency, distance):
-    test_experiment = experiments.ExperimentParametrisation()
-    structures = [
-        "1XI5",
-    ]
-    probes = [
-        indirectprobe,
-    ]
-    # prepare probe parameters to sweep
-    probe_parameters_vectors = dict(
-        target_info=[
-            dict(type="Sequence", value="EQATETQ"),
-        ],
-        labelling_efficiency=[
-            efficiency,
-        ],
-        distance_to_epitope=[
-            distance,
-        ],
-    )
-    # generate probe combinations
-    probe_parameters = sweep.create_param_combinations(**probe_parameters_vectors)
-    repetitions = 3
-    experiment, outputs, params = sweep.sweep_vasmples(
-        experiment=test_experiment,
-        structures=structures,
-        probes=probes,
-        probe_parameters=probe_parameters,
-        repetitions=repetitions,
-    )
-    assert len(outputs.keys()) > 0
-    assert len(params.keys()) > 0
 
 def test_sweep_modalities_updatemod():
     myexperiment, vsmpl_output, vsampl_pars = sweep.sweep_vasmples()
