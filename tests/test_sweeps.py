a5c76bf4ee1f7af64a8d6bd172993e1583071cc3
Damian Martinez <jdmartinezigc@gmail.com>
2025-05-21 15:01:06 +0100

    test: run analysis with defaults and plots

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -4,10 +4,4 @@ import pytest
 
 
 def test_run_analysis(sweep_gen):
-    sweep_gen._set_param_range("probe", "labelling_efficiency", "numeric", first=0.5, last=1, option=2)
-    sweep_gen._set_param_range("virtual_sample", "random_orientations", "logical", option="True")
-    sweep_gen._set_param_range("acquisition", "exp_time","numeric", first= 0.001, last = 0.1, option= 2)
-    sweep_gen.generate_acquisitions()
-    sweep_gen.generate_reference_image()
-    sweep_gen.run_analysis()
-    sweep_gen.gen_analysis_dataframe()
+    sweep_gen.run_analysis(save=True, plots=True)
