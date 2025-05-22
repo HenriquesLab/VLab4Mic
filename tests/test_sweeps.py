5d914fc0275e9bf7523d1fd630daf17fddeb5bca
Damian Martinez <jdmartinezigc@gmail.com>
2025-05-22 12:17:51 +0100

    tests: include sweep_generator method to set parameter values

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -1,7 +1,16 @@
 from supramolsim import experiments, sweep_generator
 import numpy as np
 import pytest
+import copy
 
 
-def test_run_analysis(sweep_gen):
-    sweep_gen.run_analysis(save=True, plots=True)
+def test_run_default_analysis(sweep_gen):
+    test_sweep = copy.deepcopy(sweep_gen)
+    test_sweep.run_analysis(save=True, plots=True)
+
+
+def test_set_parameter_values(sweep_gen):
+    test_sweep = copy.deepcopy(sweep_gen)
+    test_sweep.set_parameter_values("probe", "labelling_efficiency", values=(0.5,1,2))
+    test_sweep.set_parameter_values("virtual_sample", "random_orientations", values=[True, False])
+    test_sweep.run_analysis(save=True, plots=True)
