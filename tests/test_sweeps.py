ad2e3ea82b15833c786ab1166ce3f2d38e238e59
Damian Martinez <jdmartinezigc@gmail.com>
2025-05-22 16:26:24 +0100

    tests: include analysis and plot parameters

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -13,4 +13,6 @@ def test_set_parameter_values(sweep_gen):
     test_sweep = copy.deepcopy(sweep_gen)
     test_sweep.set_parameter_values("probe", "labelling_efficiency", values=(0.5,1,2))
     test_sweep.set_parameter_values("virtual_sample", "random_orientations", values=[True, False])
+    test_sweep.set_analysis_parameters("metrics_list", ["ssim", "pearson"])
+    test_sweep.set_plot_parameters("heatmaps", category="modality_name", param1="labelling_efficiency", param2="random_orientations")
     test_sweep.run_analysis(save=True, plots=True)
