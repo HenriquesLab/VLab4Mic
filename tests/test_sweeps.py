568f27b9bc0d8082da2d106fcbf3d931079f74f9
Damian Martinez <jdmartinezigc@gmail.com>
2025-05-28 11:59:57 +0100

    reduce time for testing parameter combinations

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -11,15 +11,17 @@ def test_run_default_analysis(sweep_gen):
     test_sweep.set_analysis_parameters(metrics_list = ["ssim", "pearson"])
     test_sweep.run_analysis(save=True, plots=True)
 
-def test_parameters_combinations(sweep_gen):
-    test_sweep = copy.deepcopy(sweep_gen)
-    test_sweep.set_parameter_values("probe", "labelling_efficiency", values=(0.1,1 ,3))
-    test_sweep.set_parameter_values("particle_defect", "defect", values=(0.1,0.5,3))
+def test_parameters_combinations():
+    test_sweep = sweep_generator.sweep_generator()
+    test_sweep.probes = ["CCP_heavy_chain_Cterminal",]
+    test_sweep.set_parameter_values("probe", "labelling_efficiency", values=(0.1,1 ,5))
+    test_sweep.set_parameter_values("particle_defect", "defect", values=(0.1,0.5,4))
     test_sweep.set_parameter_values("particle_defect", "defect_small_cluster", values=[300,])
     test_sweep.set_parameter_values("particle_defect", "defect_large_cluster", values=[600,])
     test_sweep.set_parameter_values("virtual_sample", "random_orientations", values=[True,False])
+    test_sweep.set_parameter_values("virtual_sample", "number_of_particles", values=[1,2,10])
 
-    param_groups = list(sweep_gen.params_by_group.keys())
+    param_groups = list(test_sweep.params_by_group.keys())
     total_combinations = 1
     for group_name in param_groups:
         if len(test_sweep.params_by_group[group_name]) > 0:
