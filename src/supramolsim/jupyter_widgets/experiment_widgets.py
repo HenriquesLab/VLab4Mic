51ad834bcc5dd2917abd037bed4dab982ee9f39a
Damian Martinez <jdmartinezigc@gmail.com>
2025-06-30 15:22:06 +0100

    refactor: update module names

--- src/supramolsim/jupyter_widgets/experiment_widgets.py
+++ src/supramolsim/jupyter_widgets/experiment_widgets.py
@@ -1,5 +1,6 @@
+from . import _experiment_parameters
 from ..experiments import ExperimentParametrisation
-from ..jupyter_widgets import parameter_selection, visualisation
+from . import _experiment_visualisation
 from ezinput import EZInput
 import os
 import sys
@@ -36,25 +37,25 @@ def _bind_widgets(parameters=None, visualisation=None):
     return gui
 
 def select_structure_widget(experiment):
-    structure_params = parameter_selection.ui_select_structure(experiment)
-    view_structure = visualisation.ui_show_structure(experiment)
+    structure_params = _experiment_parameters.ui_select_structure(experiment)
+    view_structure = _experiment_visualisation.ui_show_structure(experiment)
     select_structure = _bind_widgets(structure_params, view_structure)
     return select_structure
 
 def select_probe_widget(experiment):
-    probes_params = parameter_selection.ui_select_probe(experiment)
-    view_probes = visualisation.ui_show_labelled_structure(experiment)
+    probes_params = _experiment_parameters.ui_select_probe(experiment)
+    view_probes = _experiment_visualisation.ui_show_labelled_structure(experiment)
     select_probe = _bind_widgets(probes_params, view_probes)
     return select_probe
 
 def select_sample_parameters_widget(experiment):
-    sample_params = parameter_selection.ui_select_sample_parameters(experiment)
-    view_sample = visualisation.ui_show_virtual_sample(experiment)
+    sample_params = _experiment_parameters.ui_select_sample_parameters(experiment)
+    view_sample = _experiment_visualisation.ui_show_virtual_sample(experiment)
     select_sample = _bind_widgets(sample_params, view_sample)
     return select_sample
 
 def select_modalities_widget(experiment):
-    modalities_params = parameter_selection.ui_select_modality(experiment)
+    modalities_params = _experiment_parameters.ui_select_modality(experiment)
     select_modalities = _bind_widgets(modalities_params)
     return select_modalities
 
@@ -63,7 +64,7 @@ def select_acquisition_parameters_widget(experiment):
     """
     Create a widget to select acquisition parameters.
     """
-    view_acq_params = visualisation.ui_set_acq_params(experiment)
+    view_acq_params = _experiment_visualisation.ui_set_acq_params(experiment)
     select_acq_params = _bind_widgets(visualisation = view_acq_params)
     return select_acq_params
 
@@ -71,7 +72,7 @@ def run_experiment_widget(experiment):
     """
     Create a widget to run the experiment.
     """
-    run_experiment = parameter_selection.ui_run_experiment(experiment)
-    preview_results = visualisation.ui_preview_results(experiment)
+    run_experiment = _experiment_parameters.ui_run_experiment(experiment)
+    preview_results = _experiment_visualisation.ui_preview_results(experiment)
     run_experiment = _bind_widgets(run_experiment, preview_results)
     return run_experiment
