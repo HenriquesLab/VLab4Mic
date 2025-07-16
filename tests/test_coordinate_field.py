127d2649b6a3fc3436243ea40a680cff1b285d33
Damian Martinez <jdmartinezigc@gmail.com>
2025-07-16 17:41:55 +0100

    remove deprecated test

--- tests/test_coordinate_field.py
+++ tests/test_coordinate_field.py
@@ -27,35 +27,6 @@ def test_gen_positions_from_image(experiment_7r5k_base):
     copy_exp.use_image_for_positioning(img_mask, mode="mask", pixelsize=100)
 
 
-def test_minfield_with_particles(configuration_directory):
-    structure_id = "1XI5"
-    structure, structure_param = workflows.load_structure(
-        structure_id, configuration_directory
-    )
-    label_id = "Linker"
-    target_info = dict(
-        type="Sequence",
-        value="EQATETQ"
-    )
-    fluorophore_id = "AF647"
-    lab_eff = 1
-    tmp_label1 = data_format.structural_format.label_builder_format(
-        label_id, fluorophore_id, lab_eff, target_info
-    )
-    particle, label_params_list = workflows.particle_from_structure(
-        structure, [tmp_label1], configuration_directory
-    )
-    number_of_particles = 24
-    random_placing = True
-    # minimal distance should be calculated automatically from particle
-    coordinates_field = field.create_min_field(number_of_particles=number_of_particles,
-                                                random_placing=random_placing)
-    coordinates_field.create_molecules_from_InstanceObject(particle)
-    coordinates_field.construct_static_field()
-    exported_field = coordinates_field.export_field()
-    assert coordinates_field.molecules_params["minimal_distance"] is not None
-
-
 
 def test_nparticles_constraints():
     number_of_particles = 200
