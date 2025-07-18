f0eec5a114bf1021bc76be8a18e4f6b8a5c317c6
Damian Martinez <jdmartinezigc@gmail.com>
2025-07-18 09:40:02 +0100

    tests: include case for sequential labelling with 2 ABs

--- tests/test_sequential_labelling.py
+++ tests/test_sequential_labelling.py
@@ -18,3 +18,21 @@ def test_primary_secondary_fromExperiment():
     test_experiment.build(modules=["particle"])
     assert test_experiment.particle is not None
     assert test_experiment.exported_coordinate_field['field_emitters'] != {}
+
+
+def test_primaryAB_secondaryAB_fromExperiment():
+    sample, test_experiment = experiments.generate_virtual_sample(clear_probes=True)
+    test_experiment.remove_probes()
+    test_experiment.add_probe(
+        probe_template = "Antibody",
+        probe_target_type = "Sequence",
+        probe_target_value = "EQATETQ",
+        as_primary=True,
+        )
+    test_experiment.add_probe(
+        probe_template = "Nanobody",
+        probe_target_type = "Primary",
+        probe_target_value = "Antibody")
+    test_experiment.build(modules=["particle"])
+    assert test_experiment.particle is not None
+    assert test_experiment.exported_coordinate_field['field_emitters'] != {}
