a48ff8c33f7be009705474ac68de8ee709435813
Damian Martinez <jdmartinezigc@gmail.com>
2025-09-18 18:07:26 +0100

    capture noise images output

--- src/vlab4mic/analysis/sweep.py
+++ src/vlab4mic/analysis/sweep.py
@@ -248,7 +248,8 @@ def sweep_modalities_updatemod(
         modality_acq_prams = {}
         modality_acq_prams[0] = None
     experiment._build_imager(use_local_field=False, prints=False)
-    experiment._gen_modality_noise_images()
+    with io.capture_output() as captured:
+        experiment._gen_modality_noise_images()
     pixelsizes = dict()
     imager_scale = experiment.imager.roi_params["scale"]
     scalefactor = np.ceil(imager_scale / 1e-9)  # in nanometers
