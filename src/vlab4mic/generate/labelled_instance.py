5f41a0c4cc314b457a7eb2b370dbf61a8477e698
Damian Martinez <jdmartinezigc@gmail.com>
2025-10-01 19:01:01 +0100

    minor fix

--- src/vlab4mic/generate/labelled_instance.py
+++ src/vlab4mic/generate/labelled_instance.py
@@ -931,8 +931,9 @@ class LabeledInstance:
         numpy.ndarray or None
             Emitter coordinates or None if not found.
         """
-        if self.emitters[targetname] is not None:
-            if len(self.emitters[targetname]) == 0:
+        if self.emitters[targetname] is None:
+            return None
+        elif len(self.emitters[targetname]) == 0:
                 return None
         else:
             return self.emitters[targetname]
