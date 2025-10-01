602fa55655c70093c8adcdf345ea26926cee96d8
Damian Martinez <jdmartinezigc@gmail.com>
2025-10-01 17:59:50 +0100

    tests: test for particle rotations

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,5 +1,7 @@
 from vlab4mic import experiments
 import pytest
+import numpy as np
+import math
 
 
 def test_gen_virtual_sample():
@@ -58,3 +60,36 @@ def test_download_structure():
     modalityname = list(images.keys())[0]
     assert len(images[modalityname].shape) == 3
     assert len(list(images.keys())) == len(modalities)
+
+
+def test_vsample_function():
+    axial_offset = [0,100]
+    yz_orientations = [20,40,60,]
+    sample, experiment = experiments.generate_virtual_sample(
+        random_orientations=True,
+        random_placing=True,
+        random_rotations=True,
+        rotation_angles=[0,20,30],
+        yz_orientations=yz_orientations,
+        axial_offset=axial_offset,
+        number_of_particles=10,
+        clear_experiment=True
+    )
+    z_pos = experiment.coordinate_field.molecules[0].params["ref_point"][2]
+    axis = experiment.coordinate_field.molecules[0].axis["direction"]
+    original_axis = experiment.coordinate_field.molecules[0].axis_reset["direction"]
+    assert z_pos in axial_offset
+    original_norm = original_axis / np.linalg.norm(original_axis)
+    new_norm = axis / np.linalg.norm(axis)
+    dot_product = np.dot(original_norm, new_norm)
+    radians=np.arccos(np.clip(dot_product, -1.0, 1.0))
+    degree = math.degrees(radians)
+    found = False
+    for i in yz_orientations:
+        if (np.absolute(i - degree)) < 0.00001:
+            found = True
+    assert found
+    experiment.coordinate_field.molecules[0].reset_axis_orientation()
+    axis2 = experiment.coordinate_field.molecules[0].axis["direction"]
+    axis_diff = axis2 - original_axis
+    assert np.linalg.norm(axis_diff) < 0.00001
