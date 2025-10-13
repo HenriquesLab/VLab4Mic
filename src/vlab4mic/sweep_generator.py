3ce8a0c90c06992b9e42de2e4bfd1b687e635167
Damian Martinez <jdmartinezigc@gmail.com>
2025-10-13 15:08:35 +0100

    generate default mask for input reference

--- src/vlab4mic/sweep_generator.py
+++ src/vlab4mic/sweep_generator.py
@@ -366,6 +366,7 @@ class sweep_generator:
         if override:
             self.reference_image = ref_image
             self.reference_image_parameters = reference_parameters
+            self.reference_image_mask = np.ones(shape=ref_image[0].shape)
 
     # previews
     def preview_image_output_by_ID(
