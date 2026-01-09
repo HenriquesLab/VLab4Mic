7e0e2cd734901a8f55905eac14ec0cd7d0509fe2
Damian Martinez <jdmartinezigc@gmail.com>
2026-01-09 14:48:33 +0000

    Add text explaining how to choose previews in parameter sweep script

--- examples/parameter_sweep.py
+++ examples/parameter_sweep.py
@@ -18,14 +18,37 @@ sweep_gen = run_parameter_sweep(
     save_analysis_results=True,
     run_analysis=True
 )
+## Once our sweep has been generated, we can preview images corresponding to specific parameter combinations.
+## In order to retrieve images based on their parameters used, we use the method `preview_image_output_by_ID`,
+## which takes as input the IDs corresponding to each parameter (except for the structure) in the following order:
 
+## - probe_model
+## - probe_parameters
+## - defect_parameters
+## - virtual_sample_parameters
+## - Modality_template
+## - Modality_parameters
+## - Acquisition_parameters
+
+## You can use sweep_gen.acquisition_outputs_parameters get a dictionary where the key is
+## composed by each parameter ID separated by underscores.
+## For instance, the first combination is always "0_0_0_0_0_0_0". When a parameter is not used in the sweep,
+## its ID is always 0.
+
+## For this example, since we used 3 values for labelling efficiency and defect,
+## the possible IDs for labelling efficiency are 0, 1 or 2 (for 0, 0.5 and 1, respectively). 
+## Similarly, the IDs for defect are also 0, 1, 2 (for 0, 0.5 and 1, respectively).
+
+## We will show the simulations for SMLM modality (modality_template ID = 3) in defaults.
 fig, axs = plt.subplots(1, 2)
+# First image: labelling efficiency of 1 and no defect
 image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=2, defect_parameters=0, modality_template=3, return_image=True)
 axs[0].imshow(image, cmap="grey")
 axs[0].set_xticks([])
 axs[0].set_yticks([])
 title = "Labelling efficiency: " + str(parameters[2]["labelling_efficiency"]) + "\n" + "Incomplete Labelling: " + str(parameters[3]["defect"])
 axs[0].set_title(title)
+# Second image: labelling efficiency of 1 and defect of 0.5
 image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=2, defect_parameters=1, modality_template=3, return_image=True)
 axs[1].imshow(image, cmap="grey")
 axs[1].set_xticks([])
