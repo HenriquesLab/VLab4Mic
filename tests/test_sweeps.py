3235c1d3dbb72f56c15b808a87c154c34060202b
Damian Martinez <jdmartinezigc@gmail.com>
2026-01-09 17:53:17 +0000

    udpate test for param sweep

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -68,7 +68,7 @@ def test_run_parameter_sweep():
         defect=(0,1,2),
         defect_small_cluster=[300,],
         defect_large_cluster=[600,],
-        exp_time=[0.001, 0.01,],
+        #exp_time=[0.001, 0.01,],
         # output and analysis
         output_name="vlab_script",
         return_generator=True,
@@ -76,3 +76,15 @@ def test_run_parameter_sweep():
         save_analysis_results=True,
         run_analysis=True
         )
+    
+    assert sweep_gen_test.analysis["dataframes"] is not None
+    param_groups = list(sweep_gen_test.params_by_group.keys())
+    total_combinations = 1
+    for group_name in param_groups:
+        if len(sweep_gen_test.params_by_group[group_name]) > 0:
+            for param_name in sweep_gen_test.params_by_group[group_name]:
+                total_combinations *= len(
+                    sweep_gen_test.params_by_group[group_name][param_name]
+                )
+    vsamples_unique_ids = len(sweep_gen_test.virtual_samples_parameters.keys())
+    assert total_combinations == vsamples_unique_ids
