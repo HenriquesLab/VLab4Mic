01921a309c12af8482983461756fd61d84d39761
Damian Martinez <jdmartinezigc@gmail.com>
2026-04-10 13:55:49 +0100

    update example script for capsid

--- examples/article_figures/fig3B.py
+++ examples/article_figures/fig3B.py
@@ -13,7 +13,7 @@ sweep_gen = sweep_generator.run_parameter_sweep(
     structural_integrity=(0,1,0.25),
     structural_integrity_small_cluster=[20,],
     structural_integrity_large_cluster=[100,],
-    exp_time=[0.001, 0.01,],
+    exp_time=[0.001,],
     # output and analysis
     output_name="vlab_script",
     return_generator=True,
@@ -27,20 +27,20 @@ sweep_gen = sweep_generator.run_parameter_sweep(
 plt.rcParams['figure.figsize'] = [20, 10]
 
 fig, axs = plt.subplots(1, 4)
-image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=4, structural_integrity_parameters=2, modality_template=3, return_image=True)
-axs[0].imshow(image, cmap="grey")
+image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=4, structural_integrity_parameters=2, modality_template=3, return_image=True, replica_number=2)
+axs[0].imshow(image[50:150, 50:150], cmap="grey")
 title = "Labelling efficiency: " + str(parameters[2]["labelling_efficiency"]) + "\n" + "Structural Integrity: " + str(parameters[3]["structural_integrity"])
 axs[0].set_title(title)
-image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=1, structural_integrity_parameters=2, modality_template=3, return_image=True)
-axs[1].imshow(image, cmap="grey")
+image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=1, structural_integrity_parameters=2, modality_template=3, return_image=True, replica_number=0)
+axs[1].imshow(image[50:150, 50:150], cmap="grey")
 title = "Labelling efficiency: " + str(parameters[2]["labelling_efficiency"]) + "\n" + "Structural Integrity: " + str(parameters[3]["structural_integrity"])
 axs[1].set_title(title)
-image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=4, structural_integrity_parameters=4, modality_template=3, return_image=True)
-axs[2].imshow(image, cmap="grey")
+image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=4, structural_integrity_parameters=4, modality_template=3, return_image=True, replica_number=5)
+axs[2].imshow(image[50:150, 50:150], cmap="grey")
 title = "Labelling efficiency: " + str(parameters[2]["labelling_efficiency"]) + "\n" + "Structural Integrity: " + str(parameters[3]["structural_integrity"])
 axs[2].set_title(title)
-image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=1, structural_integrity_parameters=4, modality_template=3, return_image=True)
-axs[3].imshow(image, cmap="grey")
+image, parameters = sweep_gen.preview_image_output_by_ID(probe_parameters=3, structural_integrity_parameters=4, modality_template=3, return_image=True, replica_number=0)
+axs[3].imshow(image[50:150, 50:150], cmap="grey")
 title = "Labelling efficiency: " + str(parameters[2]["labelling_efficiency"]) + "\n" + "Structural Integrity: " + str(parameters[3]["structural_integrity"])
 axs[3].set_title(title)
 
