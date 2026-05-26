0e7ee9a79540914a95a30e71ec6000530663d575
Damian Martinez <jdmartinezigc@gmail.com>
2026-05-26 14:46:21 +0100

    Standardise scalebar in script for primary and secondary labels

--- examples/article_figures/figS_primaryandsecondary.py
+++ examples/article_figures/figS_primaryandsecondary.py
@@ -3,6 +3,8 @@ from vlab4mic.analysis import metrics
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+random_seed = 24
 
 modalities = ["STED", "SMLM",]
 
@@ -37,7 +39,8 @@ image_outputs2, image_outputs_noiseless2, experiment2 = image_vsample(
     secondary_probe = secondary,
     multimodal=modalities,
     clear_experiment=True,
-    run_simulation=True
+    run_simulation=True,
+    random_seed=random_seed
 )
 
 ## create a plot 
@@ -48,13 +51,21 @@ target_colour="#01579D"
 labelled_figure = plt.figure(figsize=[10,10])
 ###
 ax1 = labelled_figure.add_subplot(2, 2, 1, projection="3d")
-experiment1.particle.gen_axis_plot(with_sources=True, axis_object=ax1,target_colour=target_colour, source_plotsize=2, emitter_plotsize=1, source_plotalpha=1)
+experiment1.particle.gen_axis_plot(with_sources=True, axis_object=ax1,target_colour=target_colour, source_plotsize=2, emitter_plotsize=5, source_plotalpha=1)
 ax2 = labelled_figure.add_subplot(2, 2, 2)
 capsid_primary_smlm = metrics.zoom_img(image_outputs1["SMLM"]["ch0"][0], zoom_in=zoom)
 ax2.imshow(capsid_primary_smlm, cmap="grey")
 ax2.set_axis_off()
+
+length_nm = 100
+nm = 1e-09
+pixelsize = (experiment1.imaging_modalities["SMLM"]["detector"]["scale"] / nm) * experiment1.imaging_modalities["SMLM"]["detector"]["pixelsize"]
+pixelsize = np.ceil(pixelsize)
+length_px = length_nm / pixelsize
+hight_px = length_px / 10
+
 scalebar1 = AnchoredSizeBar(ax2.transData,
-                           20, '100 nm', 'lower right', 
+                           length_px, '100 nm', 'lower right', 
                            pad=2,
                            color='white',
                            frameon=False,
@@ -62,13 +73,13 @@ scalebar1 = AnchoredSizeBar(ax2.transData,
 ax2.add_artist(scalebar1)
 ### 
 ax3 = labelled_figure.add_subplot(2, 2, 3, projection="3d", sharex=ax1, sharey=ax1)
-experiment2.particle.gen_axis_plot(with_sources=True, axis_object=ax3,target_colour=target_colour, source_plotsize=2, emitter_plotsize=1, source_plotalpha=1)
+experiment2.particle.gen_axis_plot(with_sources=True, axis_object=ax3,target_colour=target_colour, source_plotsize=2, emitter_plotsize=5, source_plotalpha=1)
 ax4 = labelled_figure.add_subplot(2, 2, 4)
 capsid_primary_secondary_smlm = metrics.zoom_img(image_outputs2["SMLM"]["ch0"][0], zoom_in=zoom)
 ax4.imshow(capsid_primary_secondary_smlm, cmap="grey")
 ax4.set_axis_off()
 scalebar2 = AnchoredSizeBar(ax4.transData,
-                           20, '100 nm', 'lower right', 
+                           length_px, '100 nm', 'lower right', 
                            pad=2,
                            color='white',
                            frameon=False,
@@ -77,6 +88,7 @@ ax4.add_artist(scalebar2)
 plt.tight_layout()
 
 # save figure
-filename = os.path.join(experiment1.output_directory, 'FigureS_primaryandsecondary.png')
+name = experiment1.date_as_string + 'FigureS_primaryandsecondary.png'
+filename = os.path.join(experiment1.output_directory, name)
 labelled_figure.savefig(filename, dpi=300, bbox_inches='tight')
 plt.close()
