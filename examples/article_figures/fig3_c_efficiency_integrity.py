25c0750768aa29c11f648459f1430519080c7de5
Damian Martinez <jdmartinezigc@gmail.com>
2026-06-01 20:41:17 +0100

    update script f3c

--- examples/article_figures/fig3_c_efficiency_integrity.py
+++ examples/article_figures/fig3_c_efficiency_integrity.py
@@ -26,7 +26,7 @@ ax1 = fig.add_subplot(321)
 ax1.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax1.set_axis_off()
 ax2 = fig.add_subplot(322, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax2, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax2, emitter_plotsize=1, view_init=[90,0,0])
 
 ###### 
 myexp.set_structural_integrity(structural_integrity=0.6)
@@ -37,7 +37,7 @@ ax3 = fig.add_subplot(323)
 ax3.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax3.set_axis_off()
 ax4 = fig.add_subplot(324, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax4, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax4, emitter_plotsize=1, view_init=[90,0,0])
 #### 
 myexp.set_structural_integrity(structural_integrity=0.4)
 myexp.build(modules=["particle", "coordinate_field", "imager"])
@@ -46,7 +46,7 @@ ax5 = fig.add_subplot(325)
 ax5.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax5.set_axis_off()
 ax6 = fig.add_subplot(326, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax6, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax6, emitter_plotsize=1, view_init=[90,0,0])
 plt.tight_layout()
 
 filename = myexp.date_as_string + 'vlab4mic_fig3_panelc_integrity.png'
@@ -69,7 +69,7 @@ ax1 = fig.add_subplot(321)
 ax1.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax1.set_axis_off()
 ax2 = fig.add_subplot(322, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax2, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax2, emitter_plotsize=1, view_init=[90,0,0])
 
 ###### 
 myexp.remove_probes()
@@ -84,7 +84,7 @@ ax3 = fig.add_subplot(323)
 ax3.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax3.set_axis_off()
 ax4 = fig.add_subplot(324, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax4, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax4, emitter_plotsize=1, view_init=[90,0,0])
 #### 
 myexp.remove_probes()
 myexp.clear_labelled_structure()
@@ -97,7 +97,7 @@ ax5 = fig.add_subplot(325)
 ax5.imshow(zoom_img(images[modalities[0]]["ch0"][0], 0.6), cmap="grey")
 ax5.set_axis_off()
 ax6 = fig.add_subplot(326, projection="3d")
-myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax6, emitter_plotsize=1, view_init=[20,0,0])
+myexp.coordinate_field.molecules[0].gen_axis_plot(axis_object=ax6, emitter_plotsize=1, view_init=[90,0,0])
 plt.tight_layout()
 
 filename = myexp.date_as_string + 'vlab4mic_fig3_panelc_efficiency.png'
